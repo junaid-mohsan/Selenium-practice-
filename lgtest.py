14208e26182e67303cb70c9b1b8b8a6c57ab05f3
junaid <junaid.mohsan@arbisoft.com>
2019-12-12 01:20:11 +0500

    function is add

--- lgtest.py
+++ lgtest.py
@@ -1,9 +1,33 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import unittest
+import time
 
+class EdxLogin(unittest.TestCase): 
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:\Drivers\chromedriver_win32\chromedriver.exe")
+        
+    def test_login(self):
+        self.driver.get("https://mckinsey:academy@qa.mckinsey.edx.org/")
+        self.driver.find_element_by_css_selector('a[id="djHideToolBarButton"]').click()
 
-driver = webdriver.Chrome("C:\Drivers\chromedriver_win32\chromedriver.exe")
+        self.driver.find_element_by_css_selector('#login_id').send_keys("junaid")
 
-driver.get('https://stage.mckinsey.edx.org/')
+        self.driver.find_element_by_css_selector('button[id="login"]').click()
+        time.sleep(3)
 
+        self.driver.find_element_by_css_selector('#password').send_keys("ARbi12.,")
+        self.driver.find_element_by_css_selector('button[id="login"]').click()
+        time.sleep(3)
 
+        self.driver.find_element_by_xpath('/html/body/header/div/div/div/div/div[2]/nav/ul/li[2]/a').click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath('//*[@id="admin-body"]/header/div[1]/div/nav/ul/li[1]/a').click()
+        time.sleep(3)
+
+        self.driver.find_element_by_css_selector('input[name="search"]').send_keys("test/01/2019")
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
