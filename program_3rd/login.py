5c41dc5b06e2fb7f238cc8b94d0a6f51c95978ff
junaid <junaid.mohsan@arbisoft.com>
2020-01-08 22:28:34 +0500

    3rd assignment

--- program_3rd/login.py
+++ program_3rd/login.py
@@ -16,34 +16,32 @@ class McK_login(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_login(self):
-        self.driver.get("https://courses.edx.org")
+        self.driver.get("")
         self.assertTrue(self.login.is_browser_on_the_hpage())
         self.driver.find_element_by_link_text('Sign In').click()  
 
         #Page login  
         self.assertTrue(self.login.is_browser_on_the_loginpage())        
-        self.login.enter_username('junaid.mohsan@arbisoft.com')              
-        self.login.enter_password('Edx123@#')
+        self.login.enter_username('')              
+        self.login.enter_password('')
         self.login.submit_button()
         self.assertTrue(self.dashboard.is_browser_on_the_page())
         self.login.logout()
-        time.sleep(4)
-
-
+        
         self.assertTrue(self.register.is_browser_on_the_hpage())
         
         # Page Register    
-        # self.driver.find_element_by_link_text('Register').click()
-        # self.assertTrue(self.register.is_browser_on_the_regpage())
-        # self.register.regemail('zeezddd12@yopmail.com')
-        # self.register.regname('zeedd')
-        # self.register.regusername('zeeddzzz1')
-        # self.register.regpassword('Gillgill12!@')
-        # self.register.regcountry('Pakistan')
-        # self.register.submitButton()
-        # self.assertTrue(self.dashboard.is_browser_on_the_page())
-        # self.register.logout()
-        # self.assertTrue(self.register.is_browser_on_the_hpage())
+        self.driver.find_element_by_link_text('Register').click()
+        self.assertTrue(self.register.is_browser_on_the_regpage())
+        self.register.regemail('')
+        self.register.regname('')
+        self.register.regusername('')
+        self.register.regpassword('')
+        self.register.regcountry('')
+        self.register.submitButton()
+        self.assertTrue(self.dashboard.is_browser_on_the_page())
+        self.register.logout()
+        self.assertTrue(self.register.is_browser_on_the_hpage())
         
         
     def tearDown(self):
